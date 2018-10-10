430514bdaa4a611836b5ca6a8c7d72f2f91900a6
RePierre <petru.rebeja@gmail.com>
2018-10-10 15:58:21 +0300

    Added vanilla RNN implementation script.

---
+++ src/vanillarnn.py
@@ -0,0 +1,45 @@
+import numpy as np
+from argparse import ArgumentParser
+
+
+def read_data(input_file):
+    data = open(input_file).read()
+    return data
+
+
+def run(args):
+    data = read_data(args.corpus_file)
+    chars = list(set(data))
+    data_size, vocab_size = len(data), len(chars)
+    print("Data has {} characters; {} unique.".format(data_size, vocab_size))
+    char_to_int = {ch: i for i, ch in enumerate(chars)}
+    int_to_char = {i: ch for i, ch in enumerate(chars)}
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('--corpus-file',
+                        required=True,
+                        help='The name of the file containing training corpus.')
+    parser.add_argument('--hidden-size',
+                        required=False,
+                        type=int,
+                        default=100,
+                        help='The size of hidden layer of neurons.')
+    parser.add_argument('--sequence-length',
+                        required=False,
+                        type=int,
+                        default=25,
+                        help='Number of steps to unroll the RNN for.')
+    parser.add_argument('--learning-rate',
+                        required=False,
+                        type=float,
+                        default=1e-1,
+                        help='Learning rate.')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run(args)
