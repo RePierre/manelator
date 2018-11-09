f4e451eaab8dbcda19e3eadac56221e357f1db19
RePierre <petru.rebeja@gmail.com>
2018-11-09 16:59:58 +0200

    Split code into separate files.

--- src/vanillarnn.py
+++ src/vanillarnn.py
@@ -1,30 +1,10 @@
 import numpy as np
-from argparse import ArgumentParser
-
-
-class Encoding:
-    """
-    Encodes/Decodes the data into/from integers that will be efed to the RNN.
-    Based on the gist of Andrej Karpathy https://gist.github.com/karpathy/d4dee566867f8291f086
-    """
-
-    def __init__(self, vocabulary):
-        self._char_to_int = {}
-        self._int_to_char = {}
-        for i, ch in enumerate(vocabulary):
-            self._char_to_int[ch] = i
-            self._int_to_char[i] = ch
-
-    def encode(self, sequence):
-        return [self._char_to_int[ch] for ch in sequence]
-
-    def decode(self, sequence):
-        return "".join([self._int_to_char[i] for i in sequence])
 
 
 class VanillaRNN:
     """
     A simple implementation of Recurrent Neural Network.
+    Based on the gist of Andrej Karpathy https://gist.github.com/karpathy/d4dee566867f8291f086
     """
 
     def __init__(self, vocabulary_size, hidden_size,
@@ -138,47 +118,3 @@ class VanillaRNN:
                 sample = self._sample(inputs[0], sample_size)
                 print('-----\n{}\n-----'.format(self._encoding.decode(sample)))
             print('Epoch {}, loss: {}'.format(epoch, self._smooth_loss))
-
-
-def read_data(input_file):
-    data = open(input_file).read()
-    return data
-
-
-def run(args):
-    data = read_data(args.corpus_file)
-    chars = list(set(data))
-    data_size, vocab_size = len(data), len(chars)
-    print("Data has {} characters; {} unique.".format(data_size, vocab_size))
-    e = Encoding(chars)
-    model = VanillaRNN(vocab_size, args.hidden_size, args.sequence_length, args.learning_rate, e)
-    model.fit(data)
-
-
-def parse_arguments():
-    parser = ArgumentParser()
-    parser.add_argument('--corpus-file',
-                        required=True,
-                        help='The name of the file containing training corpus.')
-    parser.add_argument('--hidden-size',
-                        required=False,
-                        type=int,
-                        default=100,
-                        help='The size of hidden layer of neurons.')
-    parser.add_argument('--sequence-length',
-                        required=False,
-                        type=int,
-                        default=25,
-                        help='Number of steps to unroll the RNN for.')
-    parser.add_argument('--learning-rate',
-                        required=False,
-                        type=float,
-                        default=1e-1,
-                        help='Learning rate.')
-    args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = parse_arguments()
-    run(args)
