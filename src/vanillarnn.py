89e795dde7fb3900afcf1cd57344b37f19df9845
RePierre <petru.rebeja@gmail.com>
2018-11-09 18:29:24 +0200

    Started anew with implementation.

--- src/vanillarnn.py
+++ src/vanillarnn.py
@@ -1,120 +1,130 @@
 import numpy as np
 
+# data I/O
+data = open('scrapper.py', 'r').read()  # should be simple plain text file
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print('data has {} characters, {} unique.'.format(data_size, vocab_size))
+char_to_ix = {ch: i for i, ch in enumerate(chars)}
+ix_to_char = {i: ch for i, ch in enumerate(chars)}
+
+np.random.seed(0)
+
 
 class VanillaRNN:
-    """
-    A simple implementation of Recurrent Neural Network.
-    Based on the gist of Andrej Karpathy https://gist.github.com/karpathy/d4dee566867f8291f086
+    """Vanilla RNN
     """
 
-    def __init__(self, vocabulary_size, hidden_size,
-                 sequence_length, learning_rate, encoding):
-        self._vocab_size = vocabulary_size
+    def __init__(self,
+                 hidden_size=100,
+                 sequence_length=25,
+                 learning_rate=1e-1):
+
         self._hidden_size = hidden_size
         self._sequence_length = sequence_length
         self._learning_rate = learning_rate
-        self._encoding = encoding
 
-        self._initialize_model_parameters()
-        self._initialize_model_memory()
+        # model parameters
+        self._Wxh = np.random.randn(hidden_size, vocab_size) * 0.01  # input to hidden
+        self._Whh = np.random.randn(hidden_size, hidden_size) * 0.01  # hidden to hidden
+        self._Why = np.random.randn(vocab_size, hidden_size) * 0.01  # hidden to output
+        self._bh = np.zeros((hidden_size, 1))  # hidden bias
+        self._by = np.zeros((vocab_size, 1))  # output bias
 
-        self._smooth_loss = -np.log(1.0 / self._vocab_size) * self._sequence_length
-        self._hidden = np.zeros((self._hidden_size, 1))
-
-    def _initialize_model_memory(self):
-        self._mWxh = np.zeros_like(self._Wxh)
-        self._mWhh = np.zeros_like(self._Whh)
-        self._mWhy = np.zeros_like(self._Why)
-        self._mbh = np.zeros_like(self._bh)
-        self._mby = np.zeros_like(self._by)
-
-    def _initialize_model_parameters(self):
-        # Input to hidden parameters
-        self._Wxh = np.random.randn(self._hidden_size, self._vocab_size) * .01
-        # Hidden to hidden parameters
-        self._Whh = np.random.randn(self._hidden_size, self._hidden_size) * .01
-        # Hidden to output parameters
-        self._Why = np.random.randn(self._vocab_size, self._hidden_size) * .01
-        # Hidden bias
-        self._bh = np.zeros((self._hidden_size, 1))
-        # Output bias
-        self._by = np.zeros((self._vocab_size, 1))
-
-    def _sample(self, seed_index, sample_size):
-        x = np.zeros((self._vocab_size, 1))
-        x[seed_index] = 1
-        indices = []
-        h = self._hidden
-        for t in range(sample_size):
-            h = np.tanh(np.dot(self._Wxh, x) + np.dot(self._Whh, h) + self._bh)
-            y = np.dot(self._Why, h) + self._by
-            p = np.exp(y) / np.sum(np.exp(y))
-            ix = np.random.choice(range(self._vocab_size), p=p.ravel())
-            x = np.zeros((self._vocab_size, 1))
-            x[ix] = 1
-            indices.append(ix)
-
-        return indices
-
-    def _apply_loss_function(self, inputs, targets):
+    def lossFun(self, inputs, targets, hprev):
+        """
+        inputs,targets are both list of integers.
+        hprev is Hx1 array of initial hidden state
+        returns the loss, gradients on model parameters, and last hidden state
+        """
         xs, hs, ys, ps = {}, {}, {}, {}
-        hs[-1] = np.copy(self._hidden)
+        hs[-1] = np.copy(hprev)
         loss = 0
-        # Forward pass
+        # forward pass
         for t in range(len(inputs)):
-            xs[t] = np.zeros((self._vocab_size, 1))
+            xs[t] = np.zeros((vocab_size, 1))  # encode in 1-of-k representation
             xs[t][inputs[t]] = 1
-            # Hidden state
-            hs[t] = np.tanh(np.dot(self._Wxh, xs[t]) + np.dot(self._Whh, hs[t - 1]) + self._bh)
-            # Unnormalized log probabilities for next chars
-            ys[t] = np.dot(self._Why, hs[t]) + self._by
-            # Probabilities for next chars
-            ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))
-            # Cross-entropy loss
-            loss += -np.log(ps[t][targets[t], 0])
-        self._smooth_loss = self._smooth_loss * .999 + loss * .001
-        # Backward pass
+            hs[t] = np.tanh(np.dot(self._Wxh, xs[t]) + np.dot(self._Whh, hs[t - 1]) + self._bh)  # hidden state
+            ys[t] = np.dot(self._Why, hs[t]) + self._by  # unnormalized log probabilities for next chars
+            ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))  # probabilities for next chars
+            loss += -np.log(ps[t][targets[t], 0])  # softmax (cross-entropy loss)
+        # backward pass: compute gradients going backwards
         dWxh, dWhh, dWhy = np.zeros_like(self._Wxh), np.zeros_like(self._Whh), np.zeros_like(self._Why)
         dbh, dby = np.zeros_like(self._bh), np.zeros_like(self._by)
         dhnext = np.zeros_like(hs[0])
         for t in reversed(range(len(inputs))):
             dy = np.copy(ps[t])
-            dy[targets[t]] = -1
+            dy[targets[t]] -= 1  # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
             dWhy += np.dot(dy, hs[t].T)
             dby += dy
-            #  Backpropagate into h
-            dh = np.dot(self._Why.T, dy) + dhnext
-            # Backpropagate through tanh
-            dhraw = (1 - hs[t] * hs[t]) * dh
+            dh = np.dot(self._Why.T, dy) + dhnext  # backprop into h
+            dhraw = (1 - hs[t] * hs[t]) * dh  # backprop through tanh nonlinearity
             dbh += dhraw
             dWxh += np.dot(dhraw, xs[t].T)
             dWhh += np.dot(dhraw, hs[t - 1].T)
             dhnext = np.dot(self._Whh.T, dhraw)
-        # Apply clipping to mitigate exploding gradients
         for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-            np.clip(dparam, -5, 5, out=dparam)
-        return dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs) - 1]
+            np.clip(dparam, -5, 5, out=dparam)  # clip to mitigate exploding gradients
+        return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs) - 1]
 
-    def _update_parameters(self, dWxh, dWhh, dWhy, dbh, dby, hprev):
-        for param, dparam, mem in zip([self._Wxh, self._Whh, self._Why, self._bh, self._by],
-                                      [dWxh, dWhh, dWhy, dbh, dby],
-                                      [self._mWxh, self._mWhh, self._mWhy, self._mbh, self._mby]):
-            mem += dparam * dparam
-            param += -self._learning_rate * dparam / np.sqrt(mem + 1e-8)
-        self._hidden = hprev
+    def sample(self, h, seed_ix, n):
+        """
+        sample a sequence of integers from the model
+        h is memory state, seed_ix is seed letter for first time step
+        """
+        x = np.zeros((vocab_size, 1))
+        x[seed_ix] = 1
+        ixes = []
+        for t in range(n):
+            h = np.tanh(np.dot(self._Wxh, x) + np.dot(self._Whh, h) + self._bh)
+            y = np.dot(self._Why, h) + self._by
+            p = np.exp(y) / np.sum(np.exp(y))
+            ix = np.random.choice(range(vocab_size), p=p.ravel())
+            x = np.zeros((vocab_size, 1))
+            x[ix] = 1
+            ixes.append(ix)
+        return ixes
 
-    def fit(self, data, num_epochs=500, sample_interval=100, sample_size=200):
-        for epoch in range(num_epochs):
-            self._hidden = np.zeros((self._hidden_size, 1))
-            p = 0
-            while p + self._sequence_length + 1 < len(data):
-                inputs = self._encoding.encode(data[p:p + self._sequence_length])
-                labels = self._encoding.encode(data[p + 1:p + self._sequence_length + 1:])
-                dWxh, dWhh, dWhy, dbh, dby, hprev = self._apply_loss_function(inputs, labels)
-                self._update_parameters(dWxh, dWhh, dWhy, dbh, dby, hprev)
-                p += self._sequence_length
+    def fit(self):
+        n, p = 0, 0
+        mWxh, mWhh, mWhy = np.zeros_like(self._Wxh),\
+            np.zeros_like(self._Whh),\
+            np.zeros_like(self._Why)
+        # memory variables for Adagrad
+        mbh, mby = np.zeros_like(self._bh), np.zeros_like(self._by)
+        # loss at iteration 0
+        smooth_loss = -np.log(1.0 / vocab_size) * self._sequence_length
+        while True:
+            # prepare inputs (we're sweeping from left to right in steps seq_length long)
+            if p + self._sequence_length + 1 >= len(data) or n == 0:
+                hprev = np.zeros((self._hidden_size, 1))  # reset RNN memory
+                p = 0  # go from start of data
+            inputs = [char_to_ix[ch] for ch in data[p:p + self._sequence_length]]
+            targets = [char_to_ix[ch] for ch in data[p + 1:p + self._sequence_length + 1]]
 
-            if epoch % sample_interval == 0:
-                sample = self._sample(inputs[0], sample_size)
-                print('-----\n{}\n-----'.format(self._encoding.decode(sample)))
-            print('Epoch {}, loss: {}'.format(epoch, self._smooth_loss))
+            # sample from the model now and then
+            if n % 100 == 0:
+                sample_ix = self.sample(hprev, inputs[0], 200)
+                txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+                print('----\n {} \n----'.format(txt))
+
+            # forward seq_length characters through the net and fetch gradient
+            loss, dWxh, dWhh, dWhy, dbh, dby, hprev = self.lossFun(inputs, targets, hprev)
+            smooth_loss = smooth_loss * 0.999 + loss * 0.001
+            if n % 100 == 0:
+                print('iter {}, loss: {}'.format(n, smooth_loss))  # print progress
+
+            # perform parameter update with Adagrad
+            for param, dparam, mem in zip([self._Wxh, self._Whh, self._Why, self._bh, self._by],
+                                          [dWxh, dWhh, dWhy, dbh, dby],
+                                          [mWxh, mWhh, mWhy, mbh, mby]):
+                mem += dparam * dparam
+                param += -self._learning_rate * dparam / np.sqrt(mem + 1e-8)  # adagrad update
+
+            p += self._sequence_length  # move data pointer
+            n += 1  # iteration counter
+
+
+if __name__ == '__main__':
+    rnn = VanillaRNN()
+    rnn.fit()
